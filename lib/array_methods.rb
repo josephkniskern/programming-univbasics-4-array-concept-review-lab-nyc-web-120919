24f3cdaa7f7508f6cb18295f10f12db6f3d143d0
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-17 18:18:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,12 +10,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  i = 0 
-  while i < array.length - 1 
-    array.max
-    
-    i += 1 
-  end
+  return array.max
 end
 
 def find_min_value(array)
