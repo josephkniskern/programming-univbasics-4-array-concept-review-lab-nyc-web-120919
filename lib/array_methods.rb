d7521884e4cce8c103f724ebb801a9d9096e65f1
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-17 18:31:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,5 +21,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  return array.min
+  min_num = 0
+  i = 0 
+  while i < array.length 
+    min_num = array[i] if array[i] < min_num
+    i += 1 
+  end
+  
+  min_num
 end
