730e99744023f4107477251c981c14d47513e35f
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-17 18:28:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,7 +13,7 @@ def find_max_value(array)
   max_num = 0 
   i = 0 
   while i < array.length - 1 
-    max_num = array[i] if array[i]
+    max_num = array[i] if array[i] > max_num
 end
 
 def find_min_value(array)
